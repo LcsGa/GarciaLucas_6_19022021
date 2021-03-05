311089305cf8d6833ea3c79f840041a2512c9f6a
Lucas <lcs.grc@outlook.com>
2021-03-05 12:07:20 +0100

    first commit: models, routes, controllers => done

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+
+const sauceRoutes = require("./routes/sauce");
+const userRoutes = require("./routes/user");
+
+mongoose
+  .connect("mongodb+srv://LcsGa:oc-projet6@cluster0.pnp3d.mongodb.net/soPekocko?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Connexion à MongoDB Atlas réussie !"))
+  .catch(() => console.error("Connexion à MongoDB Atlas échouée !"));
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use("/api/sauces", sauceRoutes);
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
---
+++ backend/controllers/sauce.js
@@ -0,0 +1,94 @@
+const fs = require("fs");
+const Sauce = require("../models/Sauce");
+
+exports.createSauce = (req, res) => {
+  const sauceObject = JSON.parse(req.body.sauce);
+  delete sauceObject._id;
+  const sauce = new Sauce({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
+  });
+  sauce
+    .save()
+    .then(() => res.status(201).json({ message: "Sauce enregistrée !" }))
+    .catch((err) => res.status(400).json({ err }));
+};
+
+exports.getAllSauces = (_req, res) => {
+  Sauce.find()
+    .then((sauces) => res.status(200).json(sauces))
+    .catch((err) => res.status(404).json({ err }));
+};
+
+exports.getOneSauce = (req, res) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((err) => res.status(404).json({ err }));
+};
+
+const deleteSauceImage = (req, res, callback) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      const filename = sauce.imageUrl.split("/images/")[1];
+      fs.unlink(`images/${filename}`, callback);
+    })
+    .catch((err) => res.status(500).json({ err }));
+};
+
+exports.modifySauce = (req, res) => {
+  deleteSauceImage(req, res, () => {
+    const sauceObject = req.file
+      ? {
+          ...JSON.parse(req.body.sauce),
+          imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
+        }
+      : { ...req.body };
+    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+      .then(() => res.status(200).json({ message: "Sauce modifiée !" }))
+      .catch((err) => res.status(404).json({ err }));
+  });
+};
+
+exports.deleteSauce = (req, res) => {
+  deleteSauceImage(req, res, () => {
+    Sauce.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: "Sauce supprimée !" }))
+      .catch((err) => res.status(400).json({ err }));
+  });
+};
+
+exports.likeSauce = (req, res) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      let message;
+      switch (req.body.like) {
+        case 1:
+          sauce.usersLiked.push(req.body.userId);
+          message = "Sauce likée !";
+          break;
+        case -1:
+          sauce.usersDisliked.push(req.body.userId);
+          message = "Sauce dislikée !";
+          break;
+        case 0: {
+          sauce.usersLiked.includes(req.body.userId)
+            ? (sauce.usersLiked = sauce.usersLiked.filter((userId) => userId !== req.body.userId))
+            : (sauce.usersDisliked = sauce.usersDisliked.filter((userId) => userId !== req.body.userId));
+          message = "Aucun avis sur la sauce !";
+        }
+      }
+      Sauce.updateOne(
+        { _id: req.params.id },
+        {
+          usersLiked: sauce.usersLiked,
+          usersDisliked: sauce.usersDisliked,
+          likes: sauce.usersLiked.length,
+          dislikes: sauce.usersDisliked.length,
+          _id: req.params.id,
+        }
+      )
+        .then(() => res.status(201).json({ message }))
+        .catch((err) => res.status(404).json({ err }));
+    })
+    .catch((err) => res.status(500).json({ err }));
+};
