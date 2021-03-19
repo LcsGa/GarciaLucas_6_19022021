60f85e078da1df864a8c2b8f0803e4b6dd0121f3
Lucas <lcs.grc@outlook.com>
2021-03-19 14:28:32 +0100

    skip successful requests

--- backend/middleware/rateLimit.js
+++ backend/middleware/rateLimit.js
@@ -4,4 +4,5 @@ module.exports = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 5,
   message: "Vous vous êtes trompé 5 fois de mot de passe ! Réessayez dans 15 minutes !",
+  skipSuccessfulRequests: true,
 });
